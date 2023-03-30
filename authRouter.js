b7126ee262bd55564b0c4252efb6b7911b9d3471
Aleksander <asava@sibmail.com>
2023-03-30 20:12:50 +0700

    в authController реализовано registration и login

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,19 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const { check } = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post(
+    '/registration',
+    [
+        check('username', 'Имя пользователя неможет быть пустым').notEmpty(),
+        check(
+            'password',
+            'Пароль должен быть больше 4 и меньше 10 символов'
+        ).isLength({ min: 4, max: 10 })
+    ],
+    controller.registration
+)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
